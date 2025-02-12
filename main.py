bec837ae7b13df873c0bad0be25e4ea2a668b907
Trogg2000 <eldestharvie@gmail.com>
2025-02-12 05:50:22 -0800

    reading file and word counting

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(count_words(file_contents))
+def count_words(text):
+    return len(text.split())
+main()
