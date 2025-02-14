62151d58ce875643d03459f6a24fbab6f30f2c18
Trogg2000 <eldestharvie@gmail.com>
2025-02-14 07:56:26 -0800

    prettied char count readout

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def main():
     num_words = count_words(text)
     num_chars = count_chars(text)
     print(f"Words in book: {num_words}")
-    print(f"Characters in book: {num_chars}")
+    #print(f"Characters in book: {num_chars}")
+    lst = report(num_chars)
+    for item in lst:
+        print(f"The letter '{item[0]}' was found {item[1]} times")
 
 def read_text(path):
     with open(path) as f:
@@ -23,8 +26,18 @@ def count_chars(text):
             chars[char] += 1
     return chars
 
+# convert the dict into a sorted list of only letters and their count
+def report(dict):
+    lst = []
+    for k, v in dict.items():
+        if k.isalpha():
+            lst.append((k,v))
+    lst.sort(reverse= True, key=sort_key)
+    return lst
+
+def sort_key(lst):
+    return lst[1]
+
 main()
 
-# test comments
-# lorem ipsum
 
