5f5e864d1ae209f31cf033f354dab84b1b65a316
Trogg2000 <eldestharvie@gmail.com>
2025-02-13 18:33:54 -0800

    counting all chars in text

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(count_words(file_contents))
+    text = read_text("books/frankenstein.txt")
+    num_words = count_words(text)
+    num_chars = count_chars(text)
+    print(f"Words in book: {num_words}")
+    print(f"Characters in book: {num_chars}")
+
+def read_text(path):
+    with open(path) as f:
+        return f.read()
+
 def count_words(text):
     return len(text.split())
+
+def count_chars(text):
+    chars = {}
+    for char in text:
+        char = char.lower()
+        if char not in chars:
+            chars[char] = 1
+        else:
+            chars[char] += 1
+    return chars
+
 main()
+
+# test comments
+# lorem ipsum
+
